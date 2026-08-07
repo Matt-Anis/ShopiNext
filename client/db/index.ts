7d04068ffd0341acbec3567266c24fadbe404299
Matt Anis <webdev.omis@gmail.com>
2026-08-07 16:33:39 +0100

    chore: add better auth initial config

--- client/db/index.ts
+++ client/db/index.ts
@@ -1,6 +1,3 @@
-import { config } from "dotenv";
-
 import { drizzle } from "drizzle-orm/node-postgres";
-config({ path: ".env.local" });
 
-const db = drizzle(process.env.DATABASE_URL!);
+export const db = drizzle(process.env.DATABASE_URL!);
--- client/drizzle.config.ts
+++ client/drizzle.config.ts
@@ -5,7 +5,7 @@ config({ path: ".env.local" });
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./db/schema.ts",
+  schema: ["./db/schema.ts", "./lib/auth-schema.ts"],
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
