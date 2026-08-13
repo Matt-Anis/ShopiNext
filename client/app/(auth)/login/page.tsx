414ee1e492f5a383cf0996d2c6c98199872e0886
Matt Anis <webdev.omis@gmail.com>
2026-08-13 15:18:29 +0100

    style(auth): rework sign-in/sign-up UI with shared layout and floating labels

--- client/app/(auth)/login/page.tsx
+++ client/app/(auth)/login/page.tsx
@@ -1,20 +1,5 @@
-"use client"
-
-import { LoginForm } from "@/components/login-form"
-import { GalleryVerticalEndIcon } from "lucide-react"
+import { LoginForm } from "../_components/login-form";
 
 export default function LoginPage() {
-  return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-            <GalleryVerticalEndIcon className="size-4" />
-          </div>
-          Acme Inc.
-        </a>
-        <LoginForm />
-      </div>
-    </div>
-  )
+  return <LoginForm />;
 }
