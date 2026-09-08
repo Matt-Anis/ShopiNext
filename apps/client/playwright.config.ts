9a9bdd4a2cb6e5339d6048702845c2edefd4f177
Matt Anis <webdev.omis@gmail.com>
2026-09-08 17:46:50 +0100

    ci(e2e): run e2e tests against production build in CI

--- apps/client/playwright.config.ts
+++ apps/client/playwright.config.ts
@@ -24,9 +24,10 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: "npm run dev",
+    command: process.env.CI ? "npm run build && npm run start" : "npm run dev",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
+    timeout: process.env.CI ? 180_000 : 60_000,
     env: {
       ...process.env,
       DATABASE_URL: process.env.DATABASE_URL_TEST!,
